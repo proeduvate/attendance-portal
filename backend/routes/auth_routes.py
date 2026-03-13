9b9546755e139fa5c6c99fceac993b7515ae3a9b
proeduvate <proeduvate@gmail.com>
2026-03-13 18:14:48 +0530

    Update auth_routes.py

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,15 +1,51 @@
 from fastapi import APIRouter
+
 from database import users_collection
+
 from auth import verify,create_token
 
-router=APIRouter()
+
+router = APIRouter()
+
 
 @router.post("/login")
+
 def login(data:dict):
-    u=users_collection.find_one({"email":data["email"]})
-    if not u:
-        return {"error":"no user"}
-    if not verify(data["password"],u["password"]):
-        return {"error":"wrong"}
-    token=create_token({"email":u["email"],"role":u["role"]})
-    return {"token":token}
+
+    user = users_collection.find_one({
+
+        "email":data["email"]
+
+    })
+
+    if not user:
+
+        return {
+
+            "error":"User not found"
+
+        }
+
+    if not verify(data["password"],user["password"]):
+
+        return {
+
+            "error":"Invalid password"
+
+        }
+
+    token = create_token({
+
+        "email":user["email"],
+
+        "role":user["role"]
+
+    })
+
+    return {
+
+        "token":token,
+
+        "role":user["role"]
+
+    }
