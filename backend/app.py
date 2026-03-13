6e0af7011102d133ab70fef1c7c98c6ae82ce2c7
proeduvate <proeduvate@gmail.com>
2026-03-13 17:57:35 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,71 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from database import users_collection
 from auth import hash_password
-from routes import auth_routes,intern_routes,attendance_routes,upload_routes,audit_routes
 
-app=FastAPI()
+from routes import auth_routes
+from routes import intern_routes
+from routes import attendance_routes
+from routes import upload_routes
+from routes import audit_routes
 
+
+app = FastAPI()
+
+# CORS FIX (VERY IMPORTANT)
+app.add_middleware(
+
+    CORSMiddleware,
+
+    allow_origins=["*"],
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+
+)
+
+# ROUTERS
 app.include_router(auth_routes.router)
 app.include_router(intern_routes.router)
 app.include_router(attendance_routes.router)
 app.include_router(upload_routes.router)
 app.include_router(audit_routes.router)
 
+
 @app.get("/")
 def home():
-    return {"msg":"API running"}
 
-if users_collection.find_one({"email":"admin@proeduvate.com"}) is None:
-    users_collection.insert_one({
-    "email":"admin@proeduvate.com",
-    "password":hash_password("Admin@123"),
-    "role":"HR_ADMIN"
+    return {
+
+        "message":"Smart Intern Attendance System API Running"
+
+    }
+
+
+# CREATE DEFAULT ADMIN
+def create_admin():
+
+    admin = users_collection.find_one({
+
+        "email":"admin@proeduvate.com"
+
     })
+
+    if admin is None:
+
+        users_collection.insert_one({
+
+        "email":"admin@proeduvate.com",
+
+        "password":hash_password("Admin@123"),
+
+        "role":"HR_ADMIN"
+
+        })
+
+
+create_admin()
