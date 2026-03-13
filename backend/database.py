a6ffe70f117b4ab699f544e096dfae64f0ccc03c
proeduvate <proeduvate@gmail.com>
2026-03-13 12:54:31 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
-client=MongoClient("mongodb://localhost:27017")
+client=MongoClient("mongodb+srv://admin:Admin123@cluster0.ba4hjmo.mongodb.net/?appName=Cluster0")
+client=MongoClient(MONGO_URL)
 db=client["intern_attendance_db"]
 users_collection=db["users"]
 intern_collection=db["interns"]
