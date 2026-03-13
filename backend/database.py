8dd5e6619c07a72a99ca1fba9abf1fe5e8cabe3e
proeduvate <proeduvate@gmail.com>
2026-03-13 18:04:14 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,18 @@
 from pymongo import MongoClient
-client=MongoClient("mongodb+srv://admin:Admin123@cluster0.ba4hjmo.mongodb.net/?appName=Cluster0")
-client=MongoClient(MONGO_URL)
-db=client["intern_attendance_db"]
-users_collection=db["users"]
-intern_collection=db["interns"]
-attendance_collection=db["attendance"]
-audit_collection=db["audit_logs"]
-pattern_collection=db["patterns"]
+
+# MongoDB Atlas connection string
+MONGO_URL = "mongodb+srv://admin:Admin123@cluster0.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(MONGO_URL)
+
+db = client["intern_attendance_db"]
+
+users_collection = db["users"]
+
+intern_collection = db["interns"]
+
+attendance_collection = db["attendance"]
+
+audit_collection = db["audit_logs"]
+
+pattern_collection = db["patterns"]
