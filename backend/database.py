1fcd52e67c8cde7b53a5ef381e4d8ccb37d088c2
proeduvate <proeduvate@gmail.com>
2026-03-13 18:12:40 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,19 @@
 from pymongo import MongoClient
 
-# MongoDB Atlas connection string
-MONGO_URL = "mongodb+srv://admin:Admin123@cluster0.mongodb.net/?retryWrites=true&w=majority"
+# MongoDB Atlas Connection
+MONGO_URL = "mongodb+srv://admin:Admin123@cluster0.ba4hjmo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-client = MongoClient(MONGO_URL)
+client = MongoClient(
+    MONGO_URL,
+    serverSelectionTimeoutMS=5000
+)
 
 db = client["intern_attendance_db"]
 
 users_collection = db["users"]
-
 intern_collection = db["interns"]
-
 attendance_collection = db["attendance"]
-
 audit_collection = db["audit_logs"]
-
 pattern_collection = db["patterns"]
+
+print("MongoDB Connected Successfully")
