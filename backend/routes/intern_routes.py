7236c01761120972e94602159898430a80ccea14
proeduvate <proeduvate@gmail.com>
2026-03-17 00:12:38 +0530

    Update intern_routes.py

--- backend/routes/intern_routes.py
+++ backend/routes/intern_routes.py
@@ -8,86 +8,121 @@ router = APIRouter()
 
 def add_intern(data:dict):
 
-intern_collection.insert_one(data)
+    intern_collection.insert_one(data)
 
-return{
+    return{
 
-"message":"Intern Added"
+        "message":"Intern Added"
 
-}
+    }
 
 
 @router.get("/interns")
 
 def get_interns():
 
-data=list(
+    data=list(
 
-intern_collection.find(
+        intern_collection.find(
 
-{},
+            {},
 
-{"_id":0}
+            {"_id":0}
 
-)
+        )
 
-)
+    )
 
-return data
+    return data
 
 
 @router.get("/intern/{id}")
 
 def get_intern(id:str):
 
-return intern_collection.find_one(
+    return intern_collection.find_one(
 
-{"intern_id":id},
+        {"intern_id":id},
 
-{"_id":0}
+        {"_id":0}
 
-)
+    )
 
 
 @router.get("/intern_stats/{id}")
 
 def stats(id:str):
 
-data=list(
+    data=list(
 
-attendance_collection.find(
+        attendance_collection.find(
 
-{"intern_id":id}
+            {"intern_id":id}
 
-)
+        )
 
-)
+    )
 
-present=0
-absent=0
-leave=0
+    present=0
+    absent=0
+    leave=0
 
-for d in data:
+    for d in data:
 
-if d["status"]=="PRESENT":
+        if d["status"]=="PRESENT":
 
-present+=1
+            present+=1
 
-elif d["status"]=="ABSENT":
+        elif d["status"]=="ABSENT":
 
-absent+=1
+            absent+=1
 
-else:
+        else:
 
-leave+=1
+            leave+=1
 
 
-return{
+    total=present+absent+leave
 
-"present":present,
+    attendance_percent=0
 
-"absent":absent,
+    if total>0:
 
-"leave":leave
+        attendance_percent=round((present/total)*100)
 
-}
+
+    reliability=100-(absent*10)
+
+    if reliability<0:
+
+        reliability=0
+
+
+    if reliability>85:
+
+        risk="SAFE"
+
+    elif reliability>60:
+
+        risk="WARNING"
+
+    else:
+
+        risk="CRITICAL"
+
+
+    return{
+
+        "present":present,
+
+        "absent":absent,
+
+        "leave":leave,
+
+        "attendance_percent":attendance_percent,
+
+        "reliability":reliability,
+
+        "risk":risk
+
+    }
