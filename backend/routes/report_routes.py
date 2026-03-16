b30e6086f9821dbe4d0c2d1a9384ce615fec951e
proeduvate <proeduvate@gmail.com>
2026-03-17 00:38:02 +0530

    Update report_routes.py

--- backend/routes/report_routes.py
+++ backend/routes/report_routes.py
@@ -1,84 +1,64 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
-from database import intern_collection,attendance_collection
+from database import intern_collection, attendance_collection
 
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
-
-import os
 
 router = APIRouter()
 
 
 @router.get("/export/{id}")
+def export_pdf(id: str):
 
-def export_pdf(id:str):
+    intern = intern_collection.find_one(
+        {"intern_id": id},
+        {"_id": 0}
+    )
 
-intern=intern_collection.find_one(
+    attendance = list(
+        attendance_collection.find(
+            {"intern_id": id}
+        )
+    )
 
-{"intern_id":id},
+    present = 0
+    absent = 0
+    leave = 0
 
-{"_id":0}
+    for a in attendance:
 
-)
+        if a["status"] == "PRESENT":
+            present += 1
 
-attendance=list(
+        elif a["status"] == "ABSENT":
+            absent += 1
 
-attendance_collection.find(
+        else:
+            leave += 1
 
-{"intern_id":id}
+    file = f"{id}_report.pdf"
 
-)
+    c = canvas.Canvas(file, pagesize=letter)
 
-)
+    c.drawString(50, 750, "Intern Attendance Report")
 
+    c.drawString(50, 720, "Name : " + str(intern["name"]))
 
-present=0
-absent=0
-leave=0
+    c.drawString(50, 700, "ID : " + str(intern["intern_id"]))
 
-for a in attendance:
+    c.drawString(50, 680, "Department : " + str(intern["department"]))
 
-if a["status"]=="PRESENT":
+    c.drawString(50, 650, "Present : " + str(present))
 
-present+=1
+    c.drawString(50, 630, "Absent : " + str(absent))
 
-elif a["status"]=="ABSENT":
+    c.drawString(50, 610, "Leave : " + str(leave))
 
-absent+=1
+    c.save()
 
-else:
-
-leave+=1
-
-
-file=f"{id}_report.pdf"
-
-c=canvas.Canvas(file,pagesize=letter)
-
-c.drawString(50,750,"Intern Attendance Report")
-
-c.drawString(50,720,"Name : "+intern["name"])
-
-c.drawString(50,700,"ID : "+intern["intern_id"])
-
-c.drawString(50,680,"Department : "+intern["department"])
-
-c.drawString(50,650,"Present : "+str(present))
-
-c.drawString(50,630,"Absent : "+str(absent))
-
-c.drawString(50,610,"Leave : "+str(leave))
-
-c.save()
-
-
-return FileResponse(
-
-file,
-
-media_type="application/pdf",
-
-filename=file
-
-)
+    return FileResponse(
+        file,
+        media_type="application/pdf",
+        filename=file
+    )
