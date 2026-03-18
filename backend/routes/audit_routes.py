343a0e9903da373f6dc615a25fcbf8816ec92fec
proeduvate <proeduvate@gmail.com>
2026-03-18 19:57:33 +0530

    Update audit_routes.py

--- backend/routes/audit_routes.py
+++ backend/routes/audit_routes.py
@@ -1,5 +1,36 @@
 from fastapi import APIRouter
-router=APIRouter()
-@router.get("/audit_logs")
-def logs():
-    return []
+from database import audit_collection
+
+router = APIRouter()
+
+
+@router.get("/audit")
+
+def get_audit():
+
+    data=list(
+
+        audit_collection.find(
+
+            {},
+
+            {"_id":0}
+
+        )
+
+    )
+
+    return data
+
+
+@router.post("/audit")
+
+def add_audit(data:dict):
+
+    audit_collection.insert_one(data)
+
+    return{
+
+        "message":"Audit logged"
+
+    }
