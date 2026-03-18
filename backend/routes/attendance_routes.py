47d0237c34d69a535964bf2218eccbcccf3ef7fb
proeduvate <proeduvate@gmail.com>
2026-03-18 19:58:11 +0530

    Update attendance_routes.py

--- backend/routes/attendance_routes.py
+++ backend/routes/attendance_routes.py
@@ -3,6 +3,21 @@ from database import attendance_collection
 
 router=APIRouter()
 
+
 @router.get("/attendance/{intern_id}")
-def get_att(intern_id:str):
-    return list(attendance_collection.find({"intern_id":intern_id},{"_id":0}))
+
+def get_attendance(intern_id:str):
+
+    data=list(
+
+        attendance_collection.find(
+
+            {"intern_id":intern_id},
+
+            {"_id":0}
+
+        )
+
+    )
+
+    return data
